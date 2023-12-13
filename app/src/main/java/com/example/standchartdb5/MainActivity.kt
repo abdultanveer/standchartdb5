81e9cf608d5076508efd43ddb4b2c4ed40659a8f
abdul <abdul.tanveer@gmail.com>
2023-12-13 12:44:42 +0530

    view binding

--- app/src/main/java/com/example/standchartdb5/MainActivity.kt
+++ app/src/main/java/com/example/standchartdb5/MainActivity.kt
@@ -6,18 +6,33 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.AlarmClock
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import com.example.standchartdb5.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     //TextView tvMsg;
     lateinit var tvMsg: TextView
     lateinit var etName: EditText
+    lateinit var binding: ActivityMainBinding
+   // lateinit var calcButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)  //inflating xml -- parse- mem vars  in RAM
+     //   setContentView(R.layout.activity_main)  //inflating xml -- parse- mem vars  in RAM
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
         tvMsg = findViewById(R.id.tvMessage)
         etName =  findViewById(R.id.etName)
+       // calcButton = findViewById(R.id.calculate_button)
+       // calcButton.setOnClickListener {  }
+        binding.calculateButton.setOnClickListener {
+            var cost =  binding.costOfService.text.toString()
+            binding.tipResult.text = cost
+        }
     }
 
     //void clickHandler(View mview){}
@@ -25,13 +40,13 @@ class MainActivity : AppCompatActivity() {
         tvMsg.setText("welcome to android")
         //Intent hIntent = new Intent();-- intension
         var name:String  = etName.text.toString()
-        var hIntent: Intent = Intent(this,HomeActivity::class.java)
+        var hIntent: Intent = Intent(this,HomeActivity::class.java) //explicit intent
         hIntent.putExtra("nkey",name)
         startActivity(hIntent)
     }
 
     fun openDialer(view: View) {
-        var dIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:12345678"))
+        var dIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:12345678")) //implicit
         startActivity(dIntent)
     }
 
