56b8eb8c7fd613f79cf7eacd58a798004d72a5a6
abdul <abdul.tanveer@gmail.com>
2023-12-13 11:47:56 +0530

    implicit intents

--- app/src/main/java/com/example/standchartdb5/MainActivity.kt
+++ app/src/main/java/com/example/standchartdb5/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.standchartdb5
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
@@ -26,5 +28,25 @@ class MainActivity : AppCompatActivity() {
         var hIntent: Intent = Intent(this,HomeActivity::class.java)
         hIntent.putExtra("nkey",name)
         startActivity(hIntent)
+    }
+
+    fun openDialer(view: View) {
+        var dIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:12345678"))
+        startActivity(dIntent)
+    }
+
+    fun createAlarm(message: String, hour: Int, minutes: Int) {
+        val intent = Intent(AlarmClock.ACTION_SET_ALARM).apply {
+            putExtra(AlarmClock.EXTRA_MESSAGE, message)
+            putExtra(AlarmClock.EXTRA_HOUR, hour)
+            putExtra(AlarmClock.EXTRA_MINUTES, minutes)
+        }
+       // if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+       // }
+    }
+
+    fun setAlarm(view: View) {
+        createAlarm("B5 STANDARD",11,46)
     }
 }
