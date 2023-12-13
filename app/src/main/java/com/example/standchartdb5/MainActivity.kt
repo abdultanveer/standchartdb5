7b80a6fd860a93637864ecb1a8f6964869b85558
abdul <abdul.tanveer@gmail.com>
2023-12-13 09:35:09 +0530

    handle button click

--- app/src/main/java/com/example/standchartdb5/MainActivity.kt
+++ app/src/main/java/com/example/standchartdb5/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.standchartdb5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    //TextView tvMsg;
+    lateinit var tvMsg: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)  //inflating xml -- parse- mem vars  in RAM
+        tvMsg = findViewById(R.id.tvMessage)
+    }
+
+    //void clickHandler(View mview){}
+    fun clickHandler(mview: View) {
+        tvMsg.setText("welcome to android")
     }
 }
