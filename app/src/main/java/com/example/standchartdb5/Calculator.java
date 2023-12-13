3b52c43f465d712d167519cc3ec1bf29744de644
abdul <abdul.tanveer@gmail.com>
2023-12-13 16:18:04 +0530

    unit tests

---
+++ app/src/main/java/com/example/standchartdb5/Calculator.java
@@ -0,0 +1,11 @@
+package com.example.standchartdb5;
+
+public class Calculator {
+
+    public static int add(int fno, int sno){
+        return  fno + sno;
+    }
+    public static int multiply(int f, int s){
+        return  f * s;
+    }
+}
---
+++ app/src/test/java/com/example/standchartdb5/CalculatorTest.java
@@ -0,0 +1,12 @@
+package com.example.standchartdb5;
+
+import junit.framework.TestCase;
+
+public class CalculatorTest extends TestCase {
+
+    public void testAdd() {
+        int expected =  40;
+        int actual = Calculator.add(10,20);
+        assertEquals(expected,actual);
+    }
+}
