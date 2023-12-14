19476a7956c2343636e46d54b917cd4e115c17bf
abdul <abdul.tanveer@gmail.com>
2023-12-14 09:16:24 +0530

    recyclerview -2

--- app/src/main/java/com/example/standchartdb5/CountriesAdapter.kt
+++ app/src/main/java/com/example/standchartdb5/CountriesAdapter.kt
@@ -1,6 +1,33 @@
 package com.example.standchartdb5
 
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class CountriesAdapter: RecyclerView.Adapter<CViewHolder> {
+class CountriesAdapter: RecyclerView.Adapter<CViewHolder>() {
+    var TAG = CountriesAdapter::class.java.simpleName
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CViewHolder {
+        Log.i(TAG,"harsh bought a row plannk from the market and gave it to  gowtham")
+
+        var rowView =  LayoutInflater.from(parent.context).inflate(R.layout.row_plank,parent,false)
+
+        return   CViewHolder(rowView)
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: CViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
+
+
+class CViewHolder(private val rowView: View): RecyclerView.ViewHolder(rowView){
+    //Log.i(TAG,"gowtham received that row plank given by harsh")
+
+
 }
