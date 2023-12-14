4dca8cf90da729c9fed6d7c2fba8d01725f1ae1d
abdul <abdul.tanveer@gmail.com>
2023-12-14 09:29:32 +0530

    recyclerview -3

--- app/src/main/java/com/example/standchartdb5/CountriesAdapter.kt
+++ app/src/main/java/com/example/standchartdb5/CountriesAdapter.kt
@@ -4,30 +4,35 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class CountriesAdapter: RecyclerView.Adapter<CViewHolder>() {
+class CountriesAdapter(var countries: Array<String>) : RecyclerView.Adapter<CViewHolder>() {
     var TAG = CountriesAdapter::class.java.simpleName
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CViewHolder {
         Log.i(TAG,"harsh bought a row plannk from the market and gave it to  gowtham")
 
-        var rowView =  LayoutInflater.from(parent.context).inflate(R.layout.row_plank,parent,false)
+        var rowPlank =  LayoutInflater.from(parent.context).inflate(R.layout.row_plank,parent,false)
 
-        return   CViewHolder(rowView)
+        return   CViewHolder(rowPlank)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        Log.i(TAG, "mansi counted no of items --"+countries.size)
+        return countries.size
     }
 
     override fun onBindViewHolder(holder: CViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        Log.i(TAG,"ayasha is writing --"+countries[position])
+        holder.tvRow.text  = countries[position]
     }
 }
 
 
-class CViewHolder(private val rowView: View): RecyclerView.ViewHolder(rowView){
+class CViewHolder( val rowPlank: View): RecyclerView.ViewHolder(rowPlank){
     //Log.i(TAG,"gowtham received that row plank given by harsh")
+    val tvRow: TextView = rowPlank.findViewById(R.id.tvRow)
+
 
 
 }
--- app/src/main/java/com/example/standchartdb5/HomeActivity.java
+++ app/src/main/java/com/example/standchartdb5/HomeActivity.java
@@ -1,6 +1,7 @@
 package com.example.standchartdb5;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -11,7 +12,10 @@ import com.example.standchartdb5.databinding.ActivityHomeBinding;
 public class HomeActivity extends AppCompatActivity {
     TextView wTextview;
     ActivityHomeBinding homeBinding;
-    String[]  countries = {"india","usa","uk","australia"};
+    String[]  countries = {"india","usa","uk","australia",
+            "india","usa","uk","australia",
+            "india","usa","uk","australia",
+            "india","usa","uk","australia"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +24,9 @@ public class HomeActivity extends AppCompatActivity {
        /* setContentView(R.layout.activity_home);
         wTextview = findViewById(R.id.tvWelcome);*/
         //RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        CountriesAdapter adapter = new CountriesAdapter(countries);
+        homeBinding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        homeBinding.recyclerView.setAdapter(adapter);
 
         //getintent -- inntent which started this activity
        /* String dataName = getIntent().getStringExtra("nkey");
